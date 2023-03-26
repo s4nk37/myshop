8962eef077f78f50a74746a1360a78feb0efeb17
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2023-03-26 14:42:11 +0530

    Connected with firebase. Added post method to save data.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'product.dart';
 
 class Products with ChangeNotifier {
@@ -67,14 +68,29 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl);
-    _items.add(newProduct);
-    notifyListeners();
+    // const url = 'https://myshop-93710-default-rtdb.asia-southeast1.firebasedatabase.app/products.json';
+    final url = Uri.https(
+        'myshop-93710-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products.json');
+    http
+        .post(url,
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'price': product.price,
+              'imageUrl': product.imageUrl,
+              'isFavorite': product.isFavorite,
+            }))
+        .then((response) {
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
